694df30d831d1623b253977472bc231bf01bef9e
Rafael-720 <faelmknv@yahoo.com.br>
2021-11-11 01:51:43 -0200

    numero randomico
    numero randomico ja esta sorteando um valor entre 0 e 10

--- app/src/main/java/com/oliveira/rafael/sorteio/MainActivity.java
+++ app/src/main/java/com/oliveira/rafael/sorteio/MainActivity.java
@@ -3,6 +3,10 @@ package com.oliveira.rafael.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //metodo chamado pelo evento de clique no botão, para sortear o numero randomico e atribuir ao resultado
+    public void sortear(View view){
+
+        //variavel do tipo TextView atribuida ID da interface para ser manipulada internamente
+        TextView resultado = findViewById(R.id.textResultado);
+
+        //variavel que recebe um numero randomico entre 0 e 10
+        int nAleatorio = new Random().nextInt(11);
+
+        //setando a TextView para mostrar resultado
+        resultado.setText("O Número é: " + nAleatorio);
     }
 }
